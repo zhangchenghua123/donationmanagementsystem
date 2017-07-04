2bf7b913ef885649283620c0986a03eca8ddd07e
zhangchenghua123 <784283466@qq.com>
2017-07-04 14:39:38 +0800

    改了改task

--- src/sys/model/objects/Task.java
+++ src/sys/model/objects/Task.java
@@ -2,23 +2,11 @@ package sys.model.objects;
 
 /**
  * 任务类
- * 属性：任务id,任务名
+ * 属性：属性ID，任务名
  * @author 邓
  */
 public class Task {
 
-	private int taskID;
+	private int taskId;
 	private String task;
-	public int getTaskID() {
-		return taskID;
-	}
-	public void setTaskID(int taskID) {
-		this.taskID = taskID;
-	}
-	public String getTask() {
-		return task;
-	}
-	public void setTask(String task) {
-		this.task = task;
-	}
 }
